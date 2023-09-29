fdcc0bbd66f1d02585f9c8c2286ee4b37f8ee059
Galex486 <93607020+Galex486@users.noreply.github.com>
2023-09-29 11:37:36 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,16 @@
+import java.awt.color.ICC_ColorSpace;
+
 public class Main {
     public static void main(String[] args) {
 
-        TaxCalculator sale = new TaxCalculator();
-        sale.price = 10.356;
-        sale.tax = 2.564;
-        sale.quantity = 5;
-        System.out.println("I want to buy " + sale.quantity + " shirt!");
-        sale.total = sale.price * sale.quantity * sale.tax;
-        System.out.println("Total cost with tax is = " + sale.total);
+        TaxCalculator shop = new TaxCalculator();
+        shop.price = 10.356;
+        shop.tax = 2.564;
+        shop.quantity = 5;
+        System.out.println("I want to buy " + shop.quantity + " shirts!");
+        shop.total = shop.quantity * (shop.price + shop.tax);
+        System.out.println("Price = " + shop.price + " $");
+        System.out.println("Tax = " + shop.tax + " $");
+        System.out.println("Total cost with tax is = " + shop.total + " $");
     }
 }
