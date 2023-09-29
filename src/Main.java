25f2b85168ee03a3670de8055765f55170f2eead
Galex486 <93607020+Galex486@users.noreply.github.com>
2023-09-29 11:06:41 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        TaxCalculator sale = new TaxCalculator();
+        sale.price = 10.356;
+        sale.tax = 2.564;
+        sale.quantity = 5;
+        System.out.println("I want to buy " + sale.quantity + " shirt!");
+        sale.total = sale.price * sale.quantity * sale.tax;
+        System.out.println("Total cost with tax is = " + sale.total);
     }
 }
